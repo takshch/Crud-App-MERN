31caebbb4eadc9d81a0c1701762dc7de874a8a10
takshch <tkcphp@gmail.com>
2020-06-26 01:00:03 +0530

    fixed  package.json

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ app.listen(process.env.PORT || 5000, () => {
     });
 });
 
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+
 app.get("/api/", (request, response) => {
     response.send(`API ENDPOINTS:<br />
         - - - -  "/items" -- to fetch all items<br />
